66cbe6d6c5fca270f5733ba207ab35ce241cd07c
theperkinrex <theperkinrex@gmail.com>
2018-02-15 13:05:36 +0100

    Fixing

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
+
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
