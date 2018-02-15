b64219a86ef72ee6b163c2713e5306a34c8ae1e9
theperkinrex <theperkinrex@gmail.com>
2018-02-15 19:56:56 +0100

    Update description

--- setup.py
+++ setup.py
@@ -6,11 +6,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
@@ -50,7 +46,12 @@ setup(
     #
     # This field corresponds to the "Description" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-optional
-    long_description=long_description,  # Optional
+    long_description=
+    '''
+    A unit testing library for python 3 and above.
+    The wiki is at https://github.com/ThePerkinrex/Perctest/wiki
+    The project is hosted on GitHub at https://github.com/ThePerkinrex/Perctest/
+    ''',  # Optional
 
     # This should be a valid link to your project's main homepage.
     #
